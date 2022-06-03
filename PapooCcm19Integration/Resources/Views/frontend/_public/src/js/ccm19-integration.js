c06ada97f5839e48979027a8596c3503b39c0209
Christoph Grenz <cg@papoo.de>
2022-06-03 16:25:06 +0200

    Versionssprung 1.2.0 - Bessere Kompatibilität mit Shopware Consent Dialog
    - Events `onSave` und `onBuildCookiePreferences` werden ausgelöst
    - Fake `data-openconsentmanager="true"`-Element eingefügt
    - Kompatibilität mit codiverse GTM Plugin, MND Facebook-Pixel-Plugin und Tawk.to-Plugin verbessert

--- PapooCcm19Integration/Resources/Views/frontend/_public/src/js/ccm19-integration.js
+++ PapooCcm19Integration/Resources/Views/frontend/_public/src/js/ccm19-integration.js
@@ -5,12 +5,20 @@
 		return;
 	}
 
+	try {
+		if (!('_shopwareQueriedCookies' in CCM)) {
+			CCM._shopwareQueriedCookies = {};
+		}
+	} finally {
+		// ignore
+	}
+
 	var parent = ('getCookiePreference' in $ && typeof $.getCookiePreference == 'function') ? $.getCookiePreference : null;
 
 	$.getCookiePreference = function CCM19_getCookiePreference(cookieName) {
 		var result = parent ? parent(cookieName) : false;
 
-		if (CCM.acceptedCookies.includes('cookieName')) {
+		if (CCM.acceptedCookies.includes(cookieName)) {
 			result = true;
 		}
 
@@ -22,11 +30,60 @@
 			});
 		}
 
-		if (!result && cookieName === 'paypal-cookies') {
-			return CCM19_getCookiePreference('__payPalInstallmentsBannerJS_storage__');
+		if ('_shopwareQueriedCookies' in CCM) {
+			CCM._shopwareQueriedCookies[cookieName] = result;
+		}
+
+		if (!result) {
+			if (cookieName === 'paypal-cookies') {
+				return CCM19_getCookiePreference('__payPalInstallmentsBannerJS_storage__'); // Compatibility with PayPal plugin
+			}else if (cookieName == 'dtgsAllowGtmTracking') {
+				return CCM19_getCookiePreference('_gcl_au'); // Compatibility with codiverse Google Tag Manager plugin
+			}
+			else if (cookieName == 'mnd_facebook_pixel') {
+				return CCM19_getCookiePreference('_fbp'); // Compatibility with MND Facebook Pixel plugin
+			}
+			else if (cookieName == 'Tawk') {
+				return CCM19_getCookiePreference('__tawkuuid'); // Compatibility with Tawk.to
+			}
+
 		}
 
 		return result;
+	};
+
+	// Generate swCookieConsentManager events on closing the CCM19 widget
+	if ('addEventListener' in window) {
+		window.addEventListener('ccm19WidgetClosed', function () {
+			if ('swCookieConsentManager' in $.fn) {
+				var swCMObject = $.fn.swCookieConsentManager();
+				var uniqueNames = [];
+				var preferences = { groups: {CCM19: {
+					name: 'CCM19 Compatibility Layer',
+					cookies: {}
+				} }, hash: '' };
+
+				var embeddings = ('acceptedEmbeddings' in CCM) ? CCM.acceptedEmbeddings : [];
+				var cookies = CCM.acceptedCookies;
+				for (var i = 0; i < embeddings.length; ++i) {
+					uniqueNames.push(embeddings[i].name);
+					preferences.groups.CCM19.cookies[embeddings[i].name] = {name: embeddings[i].name, active: true};
+				}
+				for (var i = 0; i < cookies.length; ++i) {
+					uniqueNames.push(cookies[i]);
+					preferences.groups.CCM19.cookies[cookies[i]] = {name: cookies[i], active: true};
+				}
+
+				uniqueNames.sort();
+				preferences.hash = window.btoa(JSON.stringify(uniqueNames));
+
+				$.publish('plugin/swCookieConsentManager/onBuildCookiePreferences', [swCMObject, preferences]);
+				$.publish('plugin/swCookieConsentManager/onSave', [swCMObject]);
+			}
+		});
 	}
 
-})(jQuery, window, CCM);
+	// This is neccessary for some plugins that check for the existence of the Shopware cookie manager opener
+	$('<a href="#" aria-hidden="true" class="is--hidden" style="display: none" data-openconsentmanager="true"></a>').appendTo($('.ccm-root .ccm-settings-summoner'));
+
+})(jQuery, window, window.CCM);
